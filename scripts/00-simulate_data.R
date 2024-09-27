8286a1eb52b87be38c3393dbe7a9211f1ee18e74
Daniel Xu <danie.xu@mail.utoronto.ca>
2024-09-27 17:47:35 -0400

    Create main code Set seed Simulate data Output into csv file

--- scripts/00-simulate_data.R
+++ scripts/00-simulate_data.R
@@ -9,11 +9,37 @@
 
 
 #### Workspace setup ####
-library(tidyverse)
-# [...UPDATE THIS...]
 
-#### Simulate data ####
-# [...ADD CODE HERE...]
+library(tidyverse)
+set.seed(100)
+
+#### Define neighbourhoods and years ####
+
+neighbourhoods <- 1:148 # neighbourhood numbers (in actual data not uniform 1-148 some numbers are not included so goes up to 174)
+years <- 2013:2023 # Range of years
+
+#### Create combination of neighbourhoods and years ####
+
+data <- expand.grid(Neighbourhood = neighbourhoods, Years = years)
+data <- as_tibble(data)
+
+#### Add crime statistic columns ####
+data <- data %>%
+  mutate(
+    Population = sample(5000:10000, nrow(data), replace = TRUE),
+    Assault = sample(1:50, nrow(data), replace = TRUE),
+    Autotheft = sample(1:50, nrow(data), replace = TRUE),
+    Biketheft = sample(1:50, nrow(data), replace = TRUE),
+    Breakandenter = sample(1:50, nrow(data), replace = TRUE),
+    Homicide = sample(1:50, nrow(data), replace = TRUE),
+    Robbery = sample(1:50, nrow(data), replace = TRUE),
+    Shooting = sample(1:50, nrow(data), replace = TRUE),
+    Theftfrommv = sample(1:50, nrow(data), replace = TRUE),
+    TheftOver = sample(1:50, nrow(data), replace = TRUE),
+  )
+#### output data into file ####
+write.csv(data, file = "data/raw_data/simulated.csv")
+
 
 
 
